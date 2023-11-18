64e9a04009406b32d1cc44516e00c9bda85259e3
Justin Albert <jnalbert879@gmail.com>
2023-11-18 14:40:34 -0800

    add get ID method

---
+++ app/(tabs)/creategame/lobby.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+import { View } from 'react-native';
+import ScreenWrapperComp from '../../../components/shared/ScreenWrapperComp';
+
+const lobby: FC = () => {
+  return (
+    <ScreenWrapperComp>
+      
+    </ScreenWrapperComp>
+  )
+}
+
+export default lobby
