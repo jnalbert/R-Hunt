e019e182e1f2bdf0714d9454c7ace44bd431297c
Justin Albert <jnalbert879@gmail.com>
2023-11-18 09:01:29 -0800

    add create game tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -23,24 +23,9 @@ export default function TabLayout() {
         tabBarActiveTintColor: colors.tint,
       }}>
       <Tabs.Screen
-        name="first"
+        name="creategame"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={colors.text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          title: 'Create Game',
         }}
       />
       <Tabs.Screen
