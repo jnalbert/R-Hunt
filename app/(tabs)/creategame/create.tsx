daf2936c98993528dd29d2a2e41c1bdfc0395aef
Justin Albert <jnalbert879@gmail.com>
2023-11-18 11:18:37 -0800

    game create almost done

--- app/(tabs)/creategame/create.tsx
+++ app/(tabs)/creategame/create.tsx
@@ -1,8 +1,148 @@
-import { Text } from "react-native-paper"
-import ScreenWrapperComp from "../../../components/shared/ScreenWrapperComp"
+import { Button, Divider, Text, TextInput } from "react-native-paper";
+import ScreenWrapperComp from "../../../components/shared/ScreenWrapperComp";
+import { useAppTheme } from "../../_layout";
+import React, { FC, useEffect, useState } from "react";
+import { Alert, StyleSheet, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { MediaTypeOptions, getCameraPermissionsAsync, launchCameraAsync, requestCameraPermissionsAsync } from "expo-image-picker";
+import Objective from "../../../components/createGame/objective";
+
+const styles = StyleSheet.create({
+  wrapper: {
+    // flex: 1,
+    width: "80%",
+    justifyContent: "flex-start",
+    // alignItems: 'center',
+  },
+  objectivesWrapper: {
+    width: "100%",
+    maxHeight: 300,
+    // padding: 10,
+  },
+});
+
+export interface ObjectiveType {
+  photoUrl: string;
+  number: number;
+  longitude: number;
+  latitude: number;
+}
+
+export interface GameInfo {
+  name: string;
+  playerCount: number;
+  objectives: ObjectiveType[];
+}
 
 export default function CreateScreen() {
-  return <ScreenWrapperComp>
-    <Text>TEST</Text>
-  </ScreenWrapperComp>
+  const {
+    colors: { secondary },
+  } = useAppTheme();
+
+  const [gameInfo, setGameInfo] = useState<GameInfo>({
+    name: "",
+    playerCount: 0,
+    objectives: [],
+  });
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await requestCameraPermissionsAsync();
+    })();
+  }, []);
+
+  const onAddObjective = async () => {
+
+    const {status} = await getCameraPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("Sorry, we need camera roll permissions to make this work!");
+      return
+    }
+
+    // open the camera
+    const imageUrl = await launchCameraAsync({
+      mediaTypes: MediaTypeOptions.Images,
+      allowsMultipleSelection: false,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.5,
+    })
+    if (imageUrl.canceled) return;
+    // add a new objective to the objectives array
+    setGameInfo({
+      ...gameInfo,
+      objectives: [
+        ...gameInfo.objectives,
+        {
+          photoUrl: imageUrl.assets[0].uri,
+          number: gameInfo.objectives.length + 1,
+          longitude: 0,
+          latitude: 0,
+        },
+      ],
+    });
+    // when they take a picture
+    // get that url and add it to the objectives array with the number
+  }
+
+  return (
+    <ScreenWrapperComp>
+      <View style={styles.wrapper}>
+        <TextInput
+          mode="outlined"
+          label="Game Name"
+          value={gameInfo.name}
+          onChangeText={(text) => setGameInfo({ ...gameInfo, name: text })}
+        />
+        <TextInput
+          style={{ marginTop: 15 }}
+          mode="outlined"
+          label="Player Count"
+          value={
+            gameInfo.playerCount.toString() === "0"
+              ? ""
+              : gameInfo.playerCount.toString()
+          }
+          onChangeText={(text) =>
+            setGameInfo({ ...gameInfo, playerCount: Number(text) })
+          }
+        />
+        <Text
+          variant="titleLarge"
+          style={{ color: secondary, fontWeight: "600", marginTop: 20 }}
+        >
+          Objectives
+        </Text>
+        <Divider style={{ marginRight: 10, marginTop: 5, marginBottom: 10 }} />
+        <ScrollView style={styles.objectivesWrapper} >
+          {gameInfo.objectives.length === 0 && <Text variant="bodySmall">No objectives added</Text>}
+          {gameInfo.objectives.map((obj, index) => (
+            <Objective key={index} {...obj} />
+          ))}
+        </ScrollView>
+        <Button
+          icon="camera"
+          mode="outlined"
+          style={{ marginTop: 20, width: "60%", alignSelf: "center" }}
+          onPress={onAddObjective}
+        >
+          Add Objective
+        </Button>
+      </View>
+      <Button
+        icon="camera"
+        mode="contained"
+        style={{
+          marginTop: 20,
+          width: "60%",
+          alignSelf: "center",
+          position: "absolute",
+          bottom: 0,
+        }}
+        onPress={() => console.log("Pressed")}
+      >
+        Create Game
+      </Button>
+    </ScreenWrapperComp>
+  );
 }
---
+++ components/createGame/objective.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react'
+import { View, StyleSheet, Image } from 'react-native';
+import { ObjectiveType } from '../../app/(tabs)/creategame/create';
+import { Text } from 'react-native-paper';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    marginTop: 10,
+    marginBottom: 10,
+    display: "flex",
+    width: "80%",
+    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  objImage: {
+    // width: "100%",
+    height: 100,
+    width: 100,
+    marginLeft: 40,
+    // height: 250,
+    // backgroundColor: "red",
+  },
+});
+
+const Objective: FC<ObjectiveType> = ({number, photoUrl}) => {
+  console.log("Objective", photoUrl)
+  return (
+    <View style={styles.wrapper} >
+      <Text variant='titleLarge'># {number}</Text>
+      <Image style={styles.objImage} source={{ uri: photoUrl }} />
+    </View>
+  )
+}
+
+export default Objective;
